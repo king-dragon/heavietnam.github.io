e58f081d2996e8f8e5149f80ff1968170cce1725
king-dragon <87013210+king-dragon@users.noreply.github.com>
2022-03-06 15:43:07 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,230 @@
+// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
+// ╠╩╗║╣ ║║║ ║ ║ ║
+// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
+// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
+// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
+// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+
+const CONFIG = {
+  // ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+  // ├┴┐├─┤└─┐││  └─┐
+  // └─┘┴ ┴└─┘┴└─┘└─┘
+
+  // General
+  name: 'Heateam',
+  imageBackground: true,
+  openInNewTab: true,
+  twelveHourFormat: false,
+
+  // Greetings
+  greetingMorning: 'Good morning!',
+  greetingAfternoon: 'Good afternoon,',
+  greetingEvening: 'Good evening,',
+  greetingNight: 'Go to Sleep!',
+
+  // Layout
+  bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+
+  // Weather
+  weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+  weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+  weatherUnit: 'C', // 'F', 'C'
+  language: 'en', // More languages in https://openweathermap.org/current#multi
+
+  trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
+  defaultLatitude: '37.775',
+  defaultLongitude: '-122.419',
+
+  // Autochange
+  autoChangeTheme: true,
+
+  // Autochabge by OS
+  changeThemeByOS: true,
+
+  // Autochange by hour options (24hrs format, string must be in: hh:mm)
+  changeThemeByHour: false,
+  hourDarkThemeActive: '18:30',
+  hourDarkThemeInactive: '07:00',
+
+  // ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+  // ├┴┐│ │ │  │ │ ││││└─┐
+  // └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+  firstButtonsContainer: [
+    {
+      id: '1',
+      name: 'Github',
+      icon: 'github',
+      link: 'https://github.com/king-dragon',
+    },
+    {
+      id: '2',
+      name: 'Mail',
+      icon: 'mail',
+      link: 'https://hoanglongvonguyen@gmail.com',
+    },
+    {
+      id: '3',
+      name: 'Heavietnam',
+      icon: 'laptop',
+      link: 'https://heavietnam.ga',
+    },
+    {
+      id: '4',
+      name: 'Calendar',
+      icon: 'calendar',
+      link: 'https://calendar.google.com/calendar/r',
+    },
+    {
+      id: '5',
+      name: 'Reddit',
+      icon: 'glasses',
+      link: 'https://www.reddit.com/r/hackintosh/',
+    },
+    {
+      id: '6',
+      name: 'youtube',
+      icon: 'youtube',
+      link: 'https://www.youtube.com/channel/UCDr4F-_VOU1eAoAcu0YAFmg',
+    },
+  ],
+
+  secondButtonsContainer: [
+    {
+      id: '1',
+      name: 'Music',
+      icon: 'headphones',
+      link: 'https://open.spotify.com',
+    },
+    {
+      id: '2',
+      name: 'twitter',
+      icon: 'twitter',
+      link: 'https://twitter.com/',
+    },
+    {
+      id: '3',
+      name: 'bot',
+      icon: 'bot',
+      link: 'https://discord.com/app',
+    },
+    {
+      id: '4',
+      name: 'Amazon',
+      icon: 'shopping-bag',
+      link: 'https://amazon.com/',
+    },
+    {
+      id: '5',
+      name: 'Hashnode',
+      icon: 'pen-tool',
+      link: 'https://hashnode.com/',
+    },
+    {
+      id: '6',
+      name: 'Figma',
+      icon: 'figma',
+      link: 'https://figma.com/',
+    },
+  ],
+
+  // ┬  ┬┌─┐┌┬┐┌─┐
+  // │  │└─┐ │ └─┐
+  // ┴─┘┴└─┘ ┴ └─┘
+
+  // First Links Container
+  firstlistsContainer: [
+    {
+      icon: 'album',
+      id: '1',
+      links: [
+        {
+          name: 'EFI',
+          link: 'https://heavietnam.ga/efi-for-all-laptop/',
+        },
+        {
+          name: 'Tools',
+          link: 'https://heavietnam.ga/tool/',
+        },
+        {
+          name: 'Image',
+          link: 'https://heavietnam.ga/image/',
+        },
+        {
+          name: 'install hackintosh',
+          link:'https://heavietnam.ga/category/install-hackintosh/',
+        },
+      ],
+    },
+    {
+      icon: 'archive',
+      id: '2',
+      links: [
+        {
+          name: 'Opencore',
+          link: 'https://heavietnam.ga/bat-dau-voi-opencore/',
+        },
+        {
+          name: 'Clover',
+          link: 'https://heavietnam.ga/bat-dau-voi-clover-bootloader/',
+        },
+        {
+          name: 'Tweak',
+          link: 'https://heavietnam.ga/nhung-dieu-thu-vi-voi-maocs/',
+        },
+        {
+          name: 'Post install',
+          link: 'https://heavietnam.ga/category/post-install/',
+        },
+      ],
+    },
+  ],
+
+  // Second Links Container
+  secondListsContainer: [
+    {
+      icon: 'binary',
+      id: '1',
+      links: [
+        {
+          name: 'Spotify',
+          link: 'https://www.spotify.com',
+        },
+        {
+          name: 'Reddit',
+          link: 'https://www.reddit.com',
+        },
+        {
+          name: 'Hashnode',
+          link: 'https://www.hashnode.com',
+        },
+        {
+          name: 'Pocket',
+          link: 'https://www.pocket.com',
+        },
+      ],
+    },
+    {
+      icon: 'github',
+      id: '2',
+      links: [
+        {
+          name: 'Front',
+          link: 'https://www.reddit.com/r/Frontend/',
+        },
+        {
+          name: 'Rust',
+          link: 'https://www.reddit.com/r/rust/',
+        },
+        {
+          name: 'Go',
+          link: 'https://www.reddit.com/r/golang/',
+        },
+        {
+          name: 'Repos',
+          link: 'https://github.com/migueravila',
+        },
+      ],
+    },
+  ],
+};
